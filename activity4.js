3468372d28e79d12566caabb678b253fe69b7001
msDIZON <jasminedizon0728@gmail.com>
2025-03-29 18:50:05 +0800

    Create activity4.js

---
+++ activity4.js
@@ -0,0 +1,13 @@
+// declare a variables 
+let firstFavNum = 20;
+let secondFavNum = 28;
+
+// compare the two different variables that created using console
+console.log( firstFavNum > secondFavNum);
+console.log( firstFavNum < secondFavNum);
+console.log( firstFavNum >= secondFavNum);
+console.log( firstFavNum <= secondFavNum);
+console.log( firstFavNum === secondFavNum);
+console.log( firstFavNum == secondFavNum);
+console.log( firstFavNum !== secondFavNum);
+console.log( firstFavNum != secondFavNum);
