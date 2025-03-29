bf279edcb28fa35b8094ba52a343b7f3791b605d
msDIZON <jasminedizon0728@gmail.com>
2025-03-29 18:48:07 +0800

    Create activity3.js

---
+++ activity3.js
@@ -0,0 +1,9 @@
+// declaring variables 
+let isJsProgrammingLanguage = "true";
+let isJsHard = "false";
+let favNum = 20;
+
+// console the variables 
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(favNum + undefined);
