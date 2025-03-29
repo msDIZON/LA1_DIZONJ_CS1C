30d1568c205d1ea8d8bd24352c49a55f88014b8d
msDIZON <jasminedizon0728@gmail.com>
2025-03-29 18:50:45 +0800

    Create activity5.js

---
+++ activity5.js
@@ -0,0 +1,8 @@
+let favActorFirstName = "Soo Hyun";
+let favActorLastName = "Kim";
+let fullName = favActorFirstName + " " + favActorLastName;
+let uppercase = fullName.toUpperCase();
+let message = `My favorite actor is ${uppercase}, `;
+message += `his best drama is Queen of Tears`;
+
+console.log(message);
