12186b649f489753f1a13af97e7752bdf3febb33
msDIZON <jasminedizon0728@gmail.com>
2025-03-29 18:45:18 +0800

    Create activity2.js

---
+++ activity2.js
@@ -0,0 +1,11 @@
+// declaring a variables 
+let firstFavNum = 20;
+let secondFavNum = 28;
+
+// console using different mathematical operation 
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
