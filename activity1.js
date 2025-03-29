543eb2f91a41ebad95192e61dcc9b1e3ddff6571
msDIZON <jasminedizon0728@gmail.com>
2025-03-29 18:43:23 +0800

    Create activity1.js

---
+++ activity1.js
@@ -0,0 +1,11 @@
+// declaring a variables and store a specific data
+let name = "Jasmine";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Female";
+let twitterHandle = "@jas_mine001";
+
+// consoling the data that is stored in the variables
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
